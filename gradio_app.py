2cc7f192c25625b7429b56e678ca24746efd0322
Hugo Germain <hgermain.02@gmail.com>
2025-01-07 18:21:21 +0100

    docker ok

--- gradio_app.py
+++ gradio_app.py
@@ -1,10 +1,9 @@
 import gradio as gr 
 import requests
-from PIL import Image
 from io import BytesIO
 
 def predict_genre_via_api(image):
-    url = "http://127.0.0.1:5000/predict"  # Modifié pour docker-compose
+    url = "http://model_api:5000/predict"  # Modifié pour docker-compose
     image_binary = BytesIO()
     image.save(image_binary, format="JPEG")
     response = requests.post(url, data=image_binary.getvalue())
@@ -25,4 +24,4 @@ Interface = gr.Interface(
 # Lancer l'interface 
 
 if __name__ == "__main__":
-    Interface.launch()
+    Interface.launch(server_name="0.0.0.0", server_port=7860)
