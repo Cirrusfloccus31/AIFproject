991388e87eb1d210aaf8d42a030133aa71582f27
Hugo Germain <hgermain.02@gmail.com>
2025-01-09 11:37:04 +0100

    reformat

--- app_flask.py
+++ app_flask.py
@@ -16,20 +16,36 @@ preprocess = transforms.Compose(
     ]
 )
 
-app = Flask('Movie_genre')
+app = Flask("Movie_genre")
 
-#Download model weights
-share_url = "https://drive.google.com/file/d/1otYecurXLj7WqWjHjkE4wjOemNhKVDiU/view?usp=sharing"
+# Download model weights
+share_url = (
+    "https://drive.google.com/file/d/1otYecurXLj7WqWjHjkE4wjOemNhKVDiU/view?usp=sharing"
+)
 gdown.download(share_url, "movie_net.pth", fuzzy=True, quiet=True)
 
 # Charger le modèle
 model = load_model()
-model.load_state_dict(torch.load("movie_net.pth", weights_only=True, map_location=torch.device('cpu')))
+model.load_state_dict(
+    torch.load("movie_net.pth", weights_only=True, map_location=torch.device("cpu"))
+)
 
 # Charger les genres à partir du dataset
-genres = ['action','animation','comedy','documentary','drama','fantasy','horror','romance','science Fiction','thriller']  # Liste des genres disponibles
+genres = [
+    "action",
+    "animation",
+    "comedy",
+    "documentary",
+    "drama",
+    "fantasy",
+    "horror",
+    "romance",
+    "science Fiction",
+    "thriller",
+]  # Liste des genres disponibles
 
-@app.route('/predict', methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
     image = request.data
 
@@ -39,17 +55,20 @@ def predict():
         input_tensor = preprocess(img_pil).unsqueeze(0)  # Ajouter une dimension batch
 
         # Passer l'image dans le modèle
-        model.eval()  
+        model.eval()
         with torch.no_grad():
             output = model(input_tensor)
-        
-        predicted_index = output.argmax(dim=1).item() #obtient l'index du genre avec la proba la plus haute 
+
+        predicted_index = output.argmax(
+            dim=1
+        ).item()  # obtient l'index du genre avec la proba la plus haute
         predicted_genre = genres[predicted_index]
         print("ok")
-        return jsonify({'predicted_genre': predicted_genre}), 200
+        return jsonify({"predicted_genre": predicted_genre}), 200
 
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- gradio_app.py
+++ gradio_app.py
@@ -1,6 +1,7 @@
-import gradio as gr 
+import gradio as gr
 import requests
 from io import BytesIO
+
 
 def predict_genre_via_api(image):
     url = "http://model_api:5000/predict"  # Modifié pour docker-compose
@@ -8,20 +9,19 @@ def predict_genre_via_api(image):
     image.save(image_binary, format="JPEG")
     response = requests.post(url, data=image_binary.getvalue())
     return response.json().get("predicted_genre", response)
-    
 
 
-# Création de l'interface Gradio 
-    
+# Création de l'interface Gradio
+
 Interface = gr.Interface(
-    fn = predict_genre_via_api,
-    inputs = gr.Image(type = 'pil'),
-    outputs = "text",
-    title = 'Movie genre predictor',
-    description = 'Predict a movie genre based on its poster' 
+    fn=predict_genre_via_api,
+    inputs=gr.Image(type="pil"),
+    outputs="text",
+    title="Movie genre predictor",
+    description="Predict a movie genre based on its poster",
 )
 
-# Lancer l'interface 
+# Lancer l'interface
 
 if __name__ == "__main__":
     Interface.launch(server_name="0.0.0.0", server_port=7860)
